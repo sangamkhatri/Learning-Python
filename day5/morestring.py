337a253ee2908ac82e791d3a78471203c579e96f
Sangam Khatri <sangamkhatri420@gmail.com>
2025-05-07 19:36:28 +1000

    Day 5 learning more of strings

---
+++ day5/morestring.py
@@ -0,0 +1,23 @@
+# learning more strings manipulationa nd basic opeartions on string
+# learning to take input from the python
+# name = input("what is your name?")
+# print("hi my name is ", name)
+# slicing the string with index.
+word = "Sangamkhatriissmartandintelligent"
+print(word)
+print(word[0:10:2])
+print(word[2:10:3])
+
+unit = "Foundation os Computer programmming"
+code = "6010"
+print("hi i am studying" + unit + "and its code is" + code)
+print(len(word))
+print(word.strip())
+print(unit.replace("programming", "code"))
+# Operators
+# arithmatic Operators
+2+3  # addition
+2-3  # subtraction
+2*3  # mulitplication
+6/2  # division
+6 % 3  # modulo or remiander
