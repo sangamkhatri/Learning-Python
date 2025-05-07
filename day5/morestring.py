0b71302a2b02cd6bfc870e1467741e423d5da902
Sangam Khatri <sangamkhatri420@gmail.com>
2025-05-07 20:26:23 +1000

    some more practice with strings

--- day5/morestring.py
+++ day5/morestring.py
@@ -14,10 +14,33 @@ print("hi i am studying" + unit + "and its code is" + code)
 print(len(word))
 print(word.strip())
 print(unit.replace("programming", "code"))
-# Operators
+"""# Operators
 # arithmatic Operators
 2+3  # addition
 2-3  # subtraction
 2*3  # mulitplication
 6/2  # division
 6 % 3  # modulo or remiander
+2**3  # power/exponentiation
+15//2  # floor division (rounded)
+# Python Bitwise Operators
+2 and 3  # both has to be true or false.
+2 or 3  # one can be either true or false to justify the result.
+2 xor 3  # if both bits are same sets it to zero.
+not  # inverts all the bits so convert 1 to 0 and vice versa.
+# Python Assignment Operators
+x = 5  # it doesnt mean equals to but is assigning a value 5 to x.
+2 == 5  # double equals mean the both values are equal.
+x -= 5  # subtraction 2 with x (x=x-5)
+x += 5  # addition (x=x+5)
+x /= 5  # division (x=x/5)
+x *= 5  # mulitplication (x=x*5)
+x!=5 #x is not equals to 5
+
+#python Comaprison Operators
+==  equals to
+!= not equals to
+>= greater or equal to
+<= smaller or equal to
+"""
+num = 5:
