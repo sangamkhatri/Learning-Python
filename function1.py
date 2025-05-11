5161dd6c2520c4589572140026ab42ed80b4ffbb
Sangam Khatri <sangamkhatri420@gmail.com>
2025-05-12 00:00:03 +1000

    new update

--- function1.py
+++ function1.py
@@ -28,3 +28,63 @@ def add(a, b):
 
 
 print(add(3, 5))
+
+"""Assume the existence of a function named calculate_discounted_price with the method header below
+
+def calculate_discounted_price ( original_price: float, discount_percentage: float) -> float:
+
+Call the calculate_discounted_price function with the value 100.0 and 25.0 and save the result into the variable named final_price"""
+
+
+def calculate_discounted_price(original_price, discount_percentage):
+    if discount_percentage <= 0:
+        return original_price
+    elif discount_percentage >= 100:
+        return 0.0  # Free item
+    return (original_price * (1 - discount_percentage / 100)) * 0.5
+
+
+print(calculate_discounted_price(100.0, 25.0))
+
+"""Given the following incomplete template of is_even. Complete the function such that it takes one input (int) and returns
+True if the input is even, 
+False otherwise."""
+
+
+def is_even(n):
+    if n % 2 == 0:
+        return True
+    else:
+        return False
+
+
+print(is_even(42))
+print(is_even(-13))
+print(is_even(7))
+"""Assume the existence of two functions named foo and bar with the method headers below:
+
+def foo(n : int) -> int:
+
+def bar(n : int) -> int:
+
+Call the foo function with the value 10 and assign the result to a variable named foo_out
+
+Call the bar function with the value 3 and assign the result to a variable named bar_out.
+
+Create a variable named calculation which stores the sum of the two variables."""
+
+
+def foo(n: int) -> int:
+    return n
+
+
+def bar(n: int) -> int:
+    return n
+
+
+foo_out = foo(10)
+print(foo_out)
+bar_out = bar(3)
+print(bar_out)
+calculation = foo_out+bar_out
+print(calculation)
