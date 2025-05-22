83d6c9cefe223be33e9ca6b65dd7f4bd848f8963
Sangam Khatri <sangamkhatri420@gmail.com>
2025-05-22 23:32:25 +1000

    dictionary practice

--- practice3.py
+++ practice3.py
@@ -7,3 +7,19 @@ def print_even_numbers(numbers):
 
 my_list = [1, 2, 3, 4, 5, 6, 7, 8, 8.5]
 print_even_numbers(my_list)
+
+# Task: Create a function that prints student names and grades from a dictionary.
+
+
+def print_student_grades(student_dict):
+    for name, grade in student_dict.items():
+        print(f"{name} scored {grade}")
+
+
+students = {
+    "Sangam": 88,
+    "Abhinav": 92,
+    "Nischal": 85,
+    "Sanjeet": 90
+}
+print_student_grades(students)
