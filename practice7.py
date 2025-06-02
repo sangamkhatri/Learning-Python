892a095c2e6f06eed870afbe107cbd3ee2f7657c
Sangam Khatri <sangamkhatri420@gmail.com>
2025-06-02 22:39:26 +1000

    new files

---
+++ practice7.py
@@ -0,0 +1,19 @@
+"""Let’s create a small Python program that involves:
+
+Lists
+
+Dictionaries
+
+Classes
+
+File Input/Output (I/O)
+ Mini Project Idea: Student Grade Tracker
+We will:
+
+Create a Student class.
+
+Store multiple students in a list.
+
+Use a dictionary to map student names to grades.
+
+Read and write the student data to a file."""
